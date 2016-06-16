2bd36dac84e0cb6028fde43c0033ce3f580b1a89
randome <dejanstokanic@gmail.com>
2016-06-16 11:34:23 +0200

    Render departure flights on home show
    Right now its hard-coded data, which will be replaced with the parser

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,21 @@
 class HomeController < ApplicationController
   def show
+    render locals: {
+      departure_flights: departure_flights
+    }
+  end
+
+  private
+
+  def departure_flights
+    [OpenStruct.new({
+      scheduled_time: Time.now,
+      delayed_time: 5.minutes.from_now,
+      airline: 'JetBlue',
+      destination: 'America',
+      gate: '5G',
+      terminal: 'Terminal 3',
+      flight_status: 'Boarding'
+    })]
   end
 end
