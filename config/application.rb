5011992bacdad436c663e0bf5f8aad14451c5bcc
randome <dejanstokanic@gmail.com>
2016-06-17 10:47:08 +0200

    Set the 'lib' directory to rails autoload path

--- config/application.rb
+++ config/application.rb
@@ -31,5 +31,8 @@ module CphFlights
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+
+    # Set the 'lib' directory to rails autoload path
+    config.autoload_paths << Rails.root.join('lib')
   end
 end
