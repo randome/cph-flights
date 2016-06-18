f75f5c11d4a5427c7b3faea1838767a314115c41
randome <dejanstokanic@gmail.com>
2016-06-18 16:07:54 +0200

    HomeController is using AirportService
    To get the departure flights

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,21 +1,7 @@
 class HomeController < ApplicationController
   def show
     render locals: {
-      departure_flights: departure_flights
+      departure_flights: AirportService.departure_flights
     }
-  end
-
-  private
-
-  def departure_flights
-    [OpenStruct.new({
-      scheduled_time: Time.now,
-      delayed_time: 5.minutes.from_now,
-      airline: 'JetBlue',
-      destination: 'America',
-      gate: '5G',
-      terminal: 'Terminal 3',
-      flight_status: 'Boarding'
-    })]
   end
 end
