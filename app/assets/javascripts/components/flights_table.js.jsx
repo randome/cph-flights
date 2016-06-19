813c155c5706e9059b1d7c52538b453042d4ca35
randome <dejanstokanic@gmail.com>
2016-06-19 22:30:53 +0200

    Add Flights react component

---
+++ app/assets/javascripts/components/flights_table.js.jsx
@@ -0,0 +1,38 @@
+class FlightsTable extends React.Component {
+
+  flights() {
+    return this.props.flights.map(function(flight) {
+      return (
+        <tr>
+          <td>{ flight.scheduled_time }</td>
+          <td>{ flight.delayed_time }</td>
+          <td>{ flight.airline }</td>
+          <td>{ flight.destination }</td>
+          <td>{ flight.gate }</td>
+          <td>{ flight.terminal }</td>
+          <td>{ flight.flight_status }</td>
+        </tr>
+      );
+    }); 
+  }
+
+  render() {
+    return (<table>
+      <thead>
+        <tr>
+          <th>Scheduled Time</th>
+          <th>Delayed Time</th>
+          <th>Airline</th>
+          <th>Flying To</th>
+          <th>Gate</th>
+          <th>Terminal</th>
+          <th>Flight Status</th>
+        </tr>
+      </thead>
+      <tbody>
+        {this.flights()}
+      </tbody>
+    </table>
+    );
+  }
+}
