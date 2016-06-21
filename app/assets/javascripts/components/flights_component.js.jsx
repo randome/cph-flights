158f993f7e3d726c20ce3278057b5ddc9b9ee3a0
randome <dejanstokanic@gmail.com>
2016-06-21 06:25:10 +0200

    Updated flights_component to filter out flights on destination
    Using the new search component, for now we’ll filter out flights based
    on the destination string

--- app/assets/javascripts/components/flights_component.js.jsx
+++ app/assets/javascripts/components/flights_component.js.jsx
@@ -2,12 +2,14 @@ class FlightsComponent extends React.Component {
   constructor(props) {
     super(props);
     this.state = { flights: [] }
+    this.flightData = [];
   }
 
   componentDidMount() { 
     this.flightsRequest = $.get('/api/flights', 
         function(data) {
-          this.setState({flights: data});
+          this.setState({ flights: data });
+          this.flightData = data;
         }.bind(this)
     );
   }
@@ -16,10 +18,19 @@ class FlightsComponent extends React.Component {
     this.flightsRequest.abort();
   }
 
+  onSearchChange(inputText) {
+    filtered_flights = this.flightData.filter(function(flight) {
+      return (flight.destination.toLowerCase().indexOf(inputText.toLowerCase()) > -1) 
+    });
+
+    this.setState({ flights: filtered_flights });
+  }
+
   render() {
     return (
       <div>
         <p>Departure Flights from CPH Aiport</p>
+        <Search onChange={this.onSearchChange.bind(this)} />
         <FlightsTable flights={this.state.flights} />
       </div>
     );
